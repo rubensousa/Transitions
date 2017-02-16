2e1774b4bbc80a3ff1e2c065fed1a188c635a090
ruben <rubensousa.mieti@gmail.com>
2017-02-16 01:27:17 +0000

    Add collapsing appbar animation

--- app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
+++ app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
@@ -3,17 +3,27 @@ package com.github.rubensousa.transitions;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
 
 
 public class ToolbarActivity extends AppCompatActivity {
 
+    private AppBarLayout appbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupTransitions();
         setContentView(R.layout.activity_toolbar);
+        appbar = (AppBarLayout) findViewById(R.id.appbar);
+        appbar.post(new Runnable() {
+            @Override
+            public void run() {
+                appbar.setExpanded(true);
+            }
+        });
     }
 
     private void setupTransitions() {
@@ -22,5 +32,17 @@ public class ToolbarActivity extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    public void onBackPressed() {
+        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (verticalOffset <= -appBarLayout.getHeight() * 0.6f) {
+                    ToolbarActivity.super.onBackPressed();
+                    appBarLayout.removeOnOffsetChangedListener(this);
+                }
+            }
+        });
+        appbar.setExpanded(false);
+    }
 }
