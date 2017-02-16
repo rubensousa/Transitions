7665651c17a12fd4cec1dab62c6021041b70f296
ruben <rubensousa.mieti@gmail.com>
2017-02-16 13:37:01 +0000

    Animate nested scrollview

--- app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
+++ app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
@@ -1,16 +1,23 @@
 package com.github.rubensousa.transitions;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 
 
 public class ToolbarActivity extends AppCompatActivity {
 
-    private AppBarLayout appbar;
+    NestedScrollView nestedScrollView;
+    AppBarLayout appbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +25,22 @@ public class ToolbarActivity extends AppCompatActivity {
         setupTransitions();
         setContentView(R.layout.activity_toolbar);
         appbar = (AppBarLayout) findViewById(R.id.appbar);
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+        if (savedInstanceState == null) {
+            nestedScrollView.setAlpha(0f);
+            nestedScrollView.getViewTreeObserver()
+                    .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                        @Override
+                        public void onGlobalLayout() {
+                            if (nestedScrollView.getHeight() != 0) {
+                                nestedScrollView.animate().setStartDelay(300).alpha(1f);
+                                nestedScrollView.setTranslationY(nestedScrollView.getHeight() / 3);
+                                nestedScrollView.animate().setStartDelay(300).translationY(0)
+                                        .setInterpolator(new AccelerateDecelerateInterpolator());
+                            }
+                        }
+                    });
+        }
         /*appbar.post(new Runnable() {
             @Override
             public void run() {
@@ -34,7 +57,14 @@ public class ToolbarActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        nestedScrollView.animate().translationY(nestedScrollView.getHeight() / 3)
+                .setInterpolator(new AccelerateInterpolator()).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ToolbarActivity.super.onBackPressed();
+            }
+        });
         /*appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
