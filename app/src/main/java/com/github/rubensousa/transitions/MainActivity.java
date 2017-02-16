c954a06ec825ca325c10dded590a9298a5705ea4
ruben <rubensousa.mieti@gmail.com>
2017-02-16 00:59:53 +0000

    Add exit circular reveal

--- app/src/main/java/com/github/rubensousa/transitions/MainActivity.java
+++ app/src/main/java/com/github/rubensousa/transitions/MainActivity.java
@@ -2,31 +2,38 @@ package com.github.rubensousa.transitions;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
     private View rippleView;
     private FloatingActionButton fab;
+    private boolean launchedActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupTransitions();
         rippleView = findViewById(R.id.rippleView);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (TransitionUtils.isAtLeastLollipop()) {
-                    startRippleTransition();
+                    startRippleTransitionReveal();
                 } else {
                     startActivity();
                 }
@@ -35,12 +42,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void startRippleTransition() {
-        Animator animator = ViewAnimationUtils.createCircularReveal(rippleView, (int) fab.getX(),
+    private void startRippleTransitionReveal() {
+        fab.setVisibility(View.INVISIBLE);
+        Animator animator = ViewAnimationUtils.createCircularReveal(rippleView,
+                (int) fab.getX() + fab.getWidth() / 2,
                 (int) fab.getY(), 0, TransitionUtils.getViewRadius(rippleView));
         rippleView.setVisibility(View.VISIBLE);
         animator.setInterpolator(new AccelerateInterpolator());
-        animator.setDuration(300);
+        animator.setDuration(400);
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
@@ -51,7 +60,42 @@ public class MainActivity extends AppCompatActivity {
         animator.start();
     }
 
-    private void startActivity() {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startRippleTransitionUnreveal() {
+        Animator animator = ViewAnimationUtils.createCircularReveal(rippleView,
+                (int) fab.getX() + fab.getWidth() / 2,
+                (int) fab.getY(), TransitionUtils.getViewRadius(rippleView), 0);
+        rippleView.setVisibility(View.VISIBLE);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.setDuration(400);
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                fab.setVisibility(View.VISIBLE);
+                rippleView.setVisibility(View.INVISIBLE);
+            }
+        });
+        animator.start();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && launchedActivity) {
+            startRippleTransitionUnreveal();
+        }
+    }
+
+    private void startActivity() {
+        ActivityCompat.startActivity(this, new Intent(this, ToolbarActivity.class),
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle());
+        launchedActivity = true;
+    }
+
+    private void setupTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setExitTransition(new Fade());
+        }
     }
 }
--- app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
+++ app/src/main/java/com/github/rubensousa/transitions/ToolbarActivity.java
@@ -1,8 +1,10 @@
 package com.github.rubensousa.transitions;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
 
 
 public class ToolbarActivity extends AppCompatActivity {
@@ -10,7 +12,14 @@ public class ToolbarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupTransitions();
         setContentView(R.layout.activity_toolbar);
+    }
+
+    private void setupTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setEnterTransition(new Fade());
+        }
     }
 
 
